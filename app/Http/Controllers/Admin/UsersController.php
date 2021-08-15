8e58b766213a8157fa4e1cee3b2ffc2ab4fab50c
Alvin Faiz <softkopi3258@gmail.com>
2021-08-16 03:47:30 +0700

    users controller

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class UsersController extends Controller
+{
+    
+}
